b9b90184ca5acfbd8604fdbaa8e9b73b241106b3
sancn <official.santu05@gmail.com>
2023-05-15 11:08:23 +0545

    swagger api schema

--- bmsproj/bmsproj/urls.py
+++ bmsproj/bmsproj/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bms/',include('bmsAPP.urls')),
+    path('api_schema/', get_schema_view(
+        title='API Schema',
+        description='Guide for the REST API'
+    ), name='api_schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'api_schema'}
+        ), name='swagger-ui'),
 ]
